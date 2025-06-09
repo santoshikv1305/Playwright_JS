06b1b0666300d6d46e14f6c2a514eff3f17062d7
santoshikv1305 <santoshikv@gmail.com>
2025-06-09 08:10:52 +0530

    script updated

--- tests/UI_Test/Book_New_Flight.spec.js
+++ tests/UI_Test/Book_New_Flight.spec.js
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-
-    await page.goto("https://www.google.co.in/")
-    await page.locator("//textarea[@id='APjFqb']").fill("Plawright")
-    await page.locator("//textarea[@id='APjFqb']").click({key: "Enter"})
-})
+ 
+test('Blazedemo - Booking flight', async ({ page }) => {
+  await page.goto('https://blazedemo.com/index.php');
+  await page.locator('select[name="fromPort"]').selectOption('San Diego');
+  await page.locator('select[name="toPort"]').selectOption('Berlin');
+  await page.getByRole('button', { name: 'Find Flights' }).click();
+  await expect(page.locator('table tbody tr').first().getByRole('button', { name: 'Choose This Flight' })).toBeVisible();
+  await page.locator('table tbody tr').first().getByRole('button', { name: 'Choose This Flight' }).click();
+  await expect(page.locator('body')).toContainText('Please submit the form below to purchase the flight.');
+  await page.getByPlaceholder('First Last').fill('John Doe');
+  await page.getByPlaceholder('Main St.').fill('123 ABC Street');
+  await page.getByPlaceholder('Anytown').fill('Bangalore');
+  await page.getByPlaceholder('State').fill('KA');
+  await page.getByPlaceholder('12345').fill('1234567');
+  await page.locator('#cardType').selectOption('amex');
+  await page.getByPlaceholder('Credit Card Number').fill('123456789');
+  await page.getByPlaceholder('John Smith').fill('John Doe');
+  await page.getByRole('button', { name: 'Purchase Flight' }).click();
+  await expect(page.getByRole('heading')).toContainText('Thank you for your purchase today!');
+});
